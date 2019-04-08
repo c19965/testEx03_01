0198bb337131b4e4e24f7cc1c37c9165f9f69171
lihsin <c85123072@gmail.com>
2019-04-08 23:09:19 +0800

    Ex03_03

--- app/src/main/java/tku/testex03_01/MainActivity.java
+++ app/src/main/java/tku/testex03_01/MainActivity.java
@@ -6,11 +6,12 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextView01;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTextView01 = (TextView)findViewById(R.id.myTextView01);
+        mTextView01 = (TextView)findViewById(R.id.textView2);
         String str_2 = "test 測試 Android TextView";
         mTextView01.setText(str_2);
     }
